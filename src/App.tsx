5808f00c7636a4d835c2bf9f59a80d5414af615a
Evgenii Klepilin <evgenii.klepilin@gmail.com>
2024-05-14 15:18:13 -0500

    Add app bar and fix number type

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,17 @@
 import React, { useState } from "react";
 
-import { Box, Container, Stack, TextField, Typography } from "@mui/material";
+import {
+  Box,
+  Container,
+  Stack,
+  TextField,
+  Typography,
+  AppBar,
+  Toolbar,
+  IconButton,
+} from "@mui/material";
+
+import GitHubIcon from "@mui/icons-material/GitHub";
 
 function App() {
   const [totalAmount, setTotalAmount] = useState(0);
@@ -17,11 +28,11 @@ function App() {
   ) => {
     const amount = Number(event.target.value);
     setTotalAmount(amount);
-    setPercentage25(amount * 0.25);
-    setPercentage20(amount * 0.2);
-    setPercentage15(amount * 0.15);
-    setPercentage10(amount * 0.1);
-    setPercentageCustom(amount * customPercentage / 100);
+    setPercentage25(Number((amount * 0.25).toFixed(2)));
+    setPercentage20(Number((amount * 0.2).toFixed(2)));
+    setPercentage15(Number((amount * 0.15).toFixed(2)));
+    setPercentage10(Number((amount * 0.1).toFixed(2)));
+    setPercentageCustom(Number(((amount * customPercentage) / 100).toFixed(2)));
   };
 
   const handleCustomPercentageOnChange = (
@@ -29,82 +40,101 @@ function App() {
   ) => {
     const percentage = Number(event.target.value);
     setCustomPercentage(percentage);
-    setPercentageCustom(totalAmount * percentage / 100);
+    setPercentageCustom(
+      Number(((totalAmount * customPercentage) / 100).toFixed(2))
+    );
   };
 
   return (
-    <Box
-      display="flex"
-      justifyContent="center"
-      alignItems="center"
-      minHeight="100vh"
-    >
-      <Container maxWidth="sm">
-        <Stack spacing={3}>
-          <Typography variant="h4" textAlign="center">
-            Tip Calculator
+    <Box>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            Tip Calculator 🧮
           </Typography>
-          <Stack spacing={2}>
-            <TextField
-              variant="outlined"
-              type="number"
-              value={totalAmount}
-              label="Total Amount"
-              onChange={handleTotalAmountOnChange}
-            />
-            <TextField
-              variant="outlined"
-              value={percentage25}
-              label="25%"
-              InputProps={{
-                readOnly: true,
-              }}
-            />
-            <TextField
-              variant="outlined"
-              value={percentage20}
-              label="20%"
-              InputProps={{
-                readOnly: true,
-              }}
-            />
-            <TextField
-              variant="outlined"
-              value={percentage15}
-              label="15%"
-              InputProps={{
-                readOnly: true,
-              }}
-            />
-            <TextField
-              variant="outlined"
-              value={percentage10}
-              label="10%"
-              InputProps={{
-                readOnly: true,
-              }}
-            />
-            <Stack spacing={2} direction="row">
+          <IconButton
+            size="large"
+            color="inherit"
+            href="https://github.com/EvgeniiKlepilin/tip-calculator"
+          >
+            <GitHubIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        minHeight="90vh"
+      >
+        <Container maxWidth="sm">
+          <Stack spacing={3}>
+            <Typography variant="h4" textAlign="center">
+              Tip Calculator
+            </Typography>
+            <Stack spacing={2}>
               <TextField
                 variant="outlined"
-                value={customPercentage}
-                label="Custom %"
-                onChange={handleCustomPercentageOnChange}
-                sx={{ flex: 1 }}
+                type="tel"
+                value={totalAmount}
+                label="Total Amount"
+                onChange={handleTotalAmountOnChange}
               />
               <TextField
                 variant="outlined"
-                value={percentageCustom}
-                label={`${customPercentage}%`}
+                value={percentage25}
+                label="25%"
                 InputProps={{
                   readOnly: true,
                 }}
-                sx={{ flex: 1 }}
               />
+              <TextField
+                variant="outlined"
+                value={percentage20}
+                label="20%"
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+              <TextField
+                variant="outlined"
+                value={percentage15}
+                label="15%"
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+              <TextField
+                variant="outlined"
+                value={percentage10}
+                label="10%"
+                InputProps={{
+                  readOnly: true,
+                }}
+              />
+              <Stack spacing={2} direction="row">
+                <TextField
+                  variant="outlined"
+                  type="tel"
+                  value={customPercentage}
+                  label="Custom %"
+                  onChange={handleCustomPercentageOnChange}
+                  sx={{ flex: 1 }}
+                />
+                <TextField
+                  variant="outlined"
+                  value={percentageCustom}
+                  label={`${customPercentage}%`}
+                  InputProps={{
+                    readOnly: true,
+                  }}
+                  sx={{ flex: 1 }}
+                />
+              </Stack>
             </Stack>
           </Stack>
-        </Stack>
-      </Container>
+        </Container>
+      </Box>
     </Box>
   );
 }
